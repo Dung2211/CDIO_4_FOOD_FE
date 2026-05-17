7262dcd70ac7a6fca60854e7732326c7643b4e55
Dungdeptrai <hophuongdung2004@gmail.com>
2026-05-17 13:33:39 +0700

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/'
-})
+  base: "/CDIO_4_FOOD_FE/",
+});
